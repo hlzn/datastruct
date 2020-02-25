1b431bbb5e779ddb9a219bb4b249e92affdd4d39
Hugo Lozano <hugo@hlzn.net>
2020-02-24 23:26:08 -0600

    first

---
+++ algorithms/triplets/triplets/In.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.IO;
+
+public class In
+{
+    private static string _fileLocations = "C:\\Dev\\lab\\datastruct\\data\\";
+    public static IEnumerable<int> ReadInts(string fileName)
+    {
+        string filePath = _fileLocations + fileName;
+        using(TextReader reader = File.OpenText(filePath))
+        {
+            string lastLine;
+            while ((lastLine = reader.ReadLine()) != null)
+            {
+                yield return int.Parse(lastLine);
+            }
+        }
+    }
+}
---
+++ algorithms/triplets/triplets/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace triplets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var ints = In.ReadInts("1Kints.txt").ToArray();
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            var triplets = ThreeSum.Count(ints);
+            watch.Stop();
+            Console.WriteLine(String.Format("The number of zero sum triplets is {0} for 1Kints, process performed in {1} seconds.", triplets, watch.Elapsed));
+
+            ints = In.ReadInts("2Kints.txt").ToArray();
+            watch = new Stopwatch();
+            watch.Start();
+            triplets = ThreeSum.Count(ints);
+            watch.Stop();
+            Console.WriteLine(String.Format("The number of zero sum triplets is {0} for 2Kints, process performed in {1} seconds.", triplets, watch.Elapsed));
+
+            ints = In.ReadInts("4Kints.txt").ToArray();
+            watch = new Stopwatch();
+            watch.Start();
+            triplets = ThreeSum.Count(ints);
+            watch.Stop();
+            Console.WriteLine(String.Format("The number of zero sum triplets is {0} for 4Kints, process performed in {1} seconds.", triplets, watch.Elapsed));
+
+            ints = In.ReadInts("8Kints.txt").ToArray();
+            watch = new Stopwatch();
+            watch.Start();
+            triplets = ThreeSum.Count(ints);
+            watch.Stop();
+            Console.WriteLine(String.Format("The number of zero sum triplets is {0} for 8Kints, process performed in {1} seconds.", triplets, watch.Elapsed));
+
+            ints = In.ReadInts("16Kints.txt").ToArray();
+            watch = new Stopwatch();
+            watch.Start();
+            triplets = ThreeSum.Count(ints);
+            watch.Stop();
+            Console.WriteLine(String.Format("The number of zero sum triplets is {0} for 16Kints, process performed in {1} seconds.", triplets, watch.Elapsed));
+
+
+            Console.ReadLine();
+        }
+    }
+}
