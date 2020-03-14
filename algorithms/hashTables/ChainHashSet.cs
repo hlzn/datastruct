d456288300f5f77289dc1ba7d8409a7504442328
Hugo Lozano <hugo.lozano@unosquare.com>
2020-03-14 01:50:37 -0600

    trees and hashtables

---
+++ algorithms/hashTables/ChainHashSet.cs
@@ -0,0 +1,145 @@
+using System;
+using System.Collections.Generic;
+using queue;
+using symbolTables;
+
+namespace hashTables
+{
+    public class ChainHashSet<TKey,TValue>
+    {
+        private SequentialSearchST<TKey, TValue>[] _chains;
+        
+
+        private const int DefaultCapacity = 4;
+        public int Count { get; private set; }
+        public int Capacity { get; private set; }
+
+        public ChainHashSet(int capacity)
+        {
+            Capacity = capacity;
+            _chains = new SequentialSearchST<TKey, TValue>[capacity];
+
+            for (int i = 0; i < capacity; i++)
+            {
+                _chains[i] = new SequentialSearchST<TKey, TValue>();
+            }
+        }
+
+        public ChainHashSet():this(Prime.MinPrime)
+        {
+            
+        }
+
+        private int Hash(TKey key)
+        {
+            return key.GetHashCode() & 0x7fffffff % Capacity; 
+        }
+
+        public TValue GetValue(TKey key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(paramName: "Key is not allowed to be null");
+            }
+
+            int index = Hash(key);
+            if (_chains[index].TryGet(key, out TValue val))
+            {
+                return val;
+            }
+
+            throw new ArgumentException(message:"Key was not found");
+        }
+
+        public bool Contains(TKey key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(paramName: "Key is not allowed to be null");
+            }
+
+            int index = Hash(key);
+            return _chains[index].TryGet(key, out TValue _);
+        }
+
+        public bool Remove(TKey key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(paramName: "Key is not allowed to be null");
+            }
+
+            int index = Hash(key);
+            if (_chains[index].Contains(key))
+            {
+                Count--;
+                _chains[index].Remove(key);
+                
+                if (Capacity > DefaultCapacity && Count <= 2*Capacity)
+                {
+                    Resize(Prime.ReducePrime(Capacity));
+                }
+
+                return true;
+            } 
+            return false;
+        }
+
+        public void Add(TKey key, TValue val)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(paramName: "Key is not allowed to be null");
+            }
+
+            if (val == null)
+            {
+                Remove(key);
+                return;
+            }
+
+            if (Count >= 10*Capacity)
+            {
+                Resize(2 * Capacity);
+            }
+
+            int i = Hash(key);
+            if (!_chains[i].Contains(key))
+            {
+                Count++;
+            }
+            _chains[i].Add(key, val);
+        }
+
+        public void Resize(int chains)
+        {
+            var temp = new ChainHashSet<TKey, TValue>(chains);
+            for (int i = 0; i < Capacity; i++)
+            {
+                foreach(TKey key in _chains[i].Keys())
+                {
+                    if (_chains[i].TryGet(key, out TValue val))
+                    {
+                        temp.Add(key, val);
+                    }
+                }
+            }
+            Capacity = temp.Capacity;
+            Count = temp.Count;
+            _chains = temp._chains;
+        }
+
+        public IEnumerable<TKey> Keys()
+        {
+            var queue = new LinkedQueue<TKey>();
+            for (int i = 0; i < Capacity; i++)
+            {
+                foreach(var key in _chains[i].Keys())
+                {
+                    queue.Enqueue(key);
+                }
+            }
+            return queue;
+        }
+    }
+}
---
+++ algorithms/trees/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace trees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bstTest = new BST<int>();
+            bstTest.Insert(37);
+            bstTest.Insert(24);
+            bstTest.Insert(17);
+            bstTest.Insert(28);
+            bstTest.Insert(31);
+            bstTest.Insert(29);
+            bstTest.Insert(15);
+            bstTest.Insert(12);
+            bstTest.Insert(20);
+            
+
+            foreach(var i in bstTest.TraverseInOrder())
+            {
+                Console.Write(value:$"{i} ");
+            }
+
+            System.Console.WriteLine();
+            System.Console.WriteLine(bstTest.Min());
+            System.Console.WriteLine(bstTest.Max());
+
+            System.Console.WriteLine(bstTest.Get(value:20).Value);
+
+            
+            
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
